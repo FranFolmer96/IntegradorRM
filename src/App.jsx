bb200f89b04104d3e9d7247b4e413a454da89ebf
Your NameFranFolmer96 <hola@franfolmer.com>
2023-11-04 10:48:44 -0300

    completando integrador

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const [characters, setCharacters] = useState([]);
 
   const onSearch = (id) => {
-    axios(`https://rickandmortyapi.com/api/character/${id}`).then(
+    axios(`http://localhost:3001/rickandmorty/character/${id}`).then(
       ({ data }) => {
         if (data.name) {
           setCharacters((oldChars) => [...oldChars, data]);
